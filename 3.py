9cac46efb2a8d038723fec5f23f9ecd7e321b717
frau-muller <frau-muller@tut.by>
2021-12-20 21:12:07 +0300

    3.1

--- 3.py
+++ 3.py
@@ -25,10 +25,48 @@ data = web.DataReader(f'{crypto_currency}-{against_currency}', 'yahoo',start,end
 """
 scaler = MinMaxScaler(feature_range=(0,1))
 #преобразовываем один столбец
-scaler_data = scaler.fit_transform(data['Close'].values.reshape(-1,1))
+scaled_data = scaler.fit_transform(data['Close'].values.reshape(-1,1))
 #количество дней на основе,которых мы прогнозируем один день
 prediction_days = 40
 x_train, y_train = [], []
 #заполним фактическими значениями
 
-for x in range
+for x in range(prediction_days, len(scaled_data)):
+    x_train.append(scaled_data[x-prediction_days:x,0])
+    y_train.append(scaled_data[x,0])
+
+x_train, y_train = np.array(x_train), np.array(y_train)
+x_train = np.reshape(x_train, (x_train.shape[0],x_train.shape[1],1))
+
+#строим нейронную сеть,которая будет моделью для прогноза pip install numpy == 1.19.5
+
+model = Sequential()
+#первый слой 50 единиц
+model.add(LSTM(units=50, return_sequences=True, input_shape=(x_train.shape[1],1)))
+#модель при  выпадении 0,2 чтобы предотвратить переобучение
+model.add(Dropout(0.2))
+model.add(LSTM(units=50, return_sequences=True))
+model.add(Dropout(0.2))
+model.add(LSTM(units=50))
+#последний слой
+model.Dense(units=1)
+
+#скомпилируем
+model.compile(optimizer='adam', loss='mean_squared_error')
+#обучение модели на тестовых данных
+model.fit(x_train, y_train, epochs=25, batch_size=32)
+
+#тестируем модель с указанием временных рамок
+test_start = dt.datetime(2020,1,1)
+test_end = dt.datetime.now()
+
+test_data = web.DataReader(f'{crypto_currency}-{against_currency}', 'yahoo',test_start, test_end)
+#фактические цены будут тестовыми данными
+actual_prices = test_data['Close'].values
+
+total_dataset = pd.concat((data['Close'],test_data['Close']),axis=0)
+
+#входные данные модели будут равны общему набору данных
+model_inputs = total_dataset[len(total_dataset)-len(test_data) - prediction_days:].values
+model_inputs = m
+
